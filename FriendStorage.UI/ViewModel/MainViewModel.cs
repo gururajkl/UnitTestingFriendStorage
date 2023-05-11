1835d8dc9016d1dccecb496c7c8cee0282a7c8ee
Gururaj KL <gururajragavendra7@gmail.com>
2023-05-11 09:59:16 +0530

    Add project files.

---
+++ FriendStorage.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FriendStorage.UI.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Copyright => $"Copyrights by Friends @{DateTime.Now.Year}";
+    }
+}
